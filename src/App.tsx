658d8c38f59f384e2faa9d203b7b1ab1a7868c2d
Jeff <jbridges1119@gmail.com>
2022-09-12 16:20:28 -0600

    Add logic and api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useEffect, useState } from 'react';
 //MUI
 import { Box, styled } from '@mui/material';
 //Style
 import './App.css';
 //Components
 import BackgroundCard from './components/BackgroundCard';
+import {fetchCharacter} from './api'
+//Type
+import {Character} from './api'
 
 const Wrapper = styled(Box)`
 display: flex;
@@ -25,12 +29,41 @@ button {
   cursor: pointer;
 }
 `
-
+enum State {
+  character,
+  isLoading,
+  characterId,
+}
 
 function App() {
+  //Called "Type Casting" allows typescript to know the empty opject is a one time event and we want the type to be Character
+  const [character, setCharacter] = useState<Character>({} as Character)
+  const [isLoading, setIsLoading] = useState(false);
+  const [characterId, setCharacterId] = useState(1)
+
+useEffect(() => {
+  const fetchFromApi = async () => {
+    setIsLoading(true);
+    const result = await fetchCharacter(characterId)
+    setIsLoading(false)
+    setCharacter(result)
+    console.log(result)
+  }
+  fetchFromApi()
+}, [characterId])
+
   return (
     <Wrapper>
-      <BackgroundCard name='Jeff' imgUrl='url'/>
+      {isLoading ? (
+        <p>Loading...</p>
+      ):(<>
+<BackgroundCard name={character.name} imgUrl={character.img_url}/>
+<button onClick={() => setCharacterId(Math.floor(Math.random() * 10) + 1)}>
+  Random Character
+</button>
+</>
+      )}
+      
     </Wrapper>
   );
 }
